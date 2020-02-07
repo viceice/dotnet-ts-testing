52dd95cbdf7ac4fc58af08c1960095fd123de01e
Michael Kriese <michael.kriese@visualon.de>
2020-02-07 09:18:33 +0100

    feat: update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,45 @@
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
 
-export default {
+const base = {
   input: "scripts/tsc.ts",
-  output: [
-    {
-      file: "scripts/esm/tsc.js",
-      format: "esm",
-    },
-    {
-      file: "scripts/tsc.js",
-      format: "cjs",
-      exports: 'named'
-    },
-  ],
   plugins: [
     resolve({ browser: true }),
-    typescript({
-      tsconfig: "tsconfig.scripts.json",
-    }),
     commonjs(),
+    typescript({
+      tsconfig: "tsconfig.cjs.json",
+    }),
   ],
 };
+
+const esm = {
+  file: "scripts/tsc.esm.js",
+  format: "esm",
+};
+const cjs = {
+  file: "scripts/tsc.js",
+  format: "cjs",
+  exports: "named",
+};
+
+export default [
+  {
+    ...base,
+    output: [
+      cjs,
+      {
+        ...cjs,
+        file: "scripts/tsc.min.js",
+        plugins: [terser()],
+      },
+      esm,
+      {
+        ...esm,
+        file: "scripts/tsc.esm.min.js",
+        plugins: [terser()],
+      },
+    ],
+  },
+];
