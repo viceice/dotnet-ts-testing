0ed0247f0099b1371c9973f70c6838e9ee07b1b8
Michael Kriese <michael.kriese@visualon.de>
2021-07-20 10:19:49 +0200

    fix: ts 4.x changes

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,29 @@
-import typescript from "@rollup/plugin-typescript";
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
 
 const base = {
-  input: "scripts/tsc.ts",
+  input: 'scripts/tsc.ts',
   plugins: [
-    resolve({ browser: true }),
+    nodeResolve({ browser: true, preferBuiltins: false }),
     commonjs(),
+    nodePolyfills({ include: ['perf-hooks'] }),
     typescript({
-      tsconfig: "tsconfig.cjs.json",
+      tsconfig: 'tsconfig.cjs.json',
     }),
   ],
 };
 
 const esm = {
-  file: "scripts/tsc.esm.js",
-  format: "esm",
+  file: 'scripts/tsc.esm.js',
+  format: 'esm',
 };
 const cjs = {
-  file: "scripts/tsc.js",
-  format: "cjs",
-  exports: "named",
+  file: 'scripts/tsc.js',
+  format: 'cjs',
+  exports: 'named',
 };
 
 export default [
@@ -31,13 +33,13 @@ export default [
       cjs,
       {
         ...cjs,
-        file: "scripts/tsc.min.js",
+        file: 'scripts/tsc.min.js',
         plugins: [terser()],
       },
       esm,
       {
         ...esm,
-        file: "scripts/tsc.esm.min.js",
+        file: 'scripts/tsc.esm.min.js',
         plugins: [terser()],
       },
     ],
