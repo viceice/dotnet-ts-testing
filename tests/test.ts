665726ec52bbb05c4676aa5a668550d78091f88b
Michael Kriese <michael.kriese@visualon.de>
2021-12-01 11:59:25 +0100

    test: add class test

--- tests/test.ts
+++ tests/test.ts
@@ -2,3 +2,9 @@ import console from 'console';
 export const i = 5;
 
 console?.log('test');
+
+export default class Test {
+  static inject = [console];
+  constructor(private ss: console) {
+  }
+}
